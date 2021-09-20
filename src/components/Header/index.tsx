e1dde85fb0acc71928e0d344310cc66d0c7adb83
Felipe da Costa <felipecfvb@gmail.com>
2021-09-20 18:48:16 -0300

    Adicionando Menu Hamburguer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,8 @@
+import React from 'react';
 import "./Header.css";
 import logo from "../../assets/logo.png";
+import openMenuImg from "../../assets/menu.png"
+import closeMenuImg from "../../assets/close.png"
 
 function Header() {
   return (
@@ -8,14 +11,32 @@ function Header() {
         <img src={logo} alt="" />
       </div>
       <div className="header-nav-links">
-          <ul className="header-menu">
-              <a href="/"><li>Home</li></a>
-              <a href="/sobre"><li>Sobre</li></a>
-              <a href="/login"><li>Curso</li></a>
-          </ul>
+          <div className="header-menu">
+              <a href="/">Home</a>
+              <a href="/sobre">Sobre</a>
+              <a href="/login">Curso</a>
+          </div>
       </div>
+
+      <div className="menu-mobile">
+      <img src={openMenuImg} alt="" />
+      <div className="menu-hamburguer">
+        <div className="menu-hamburguer-close">
+        <img src={closeMenuImg} alt="" />
+        </div>
+        <div className="menu-hamburguer-links">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Projeto</a></li>
+            <li><a href="#">Livro</a></li>
+            <li><a href="#">Curso</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
     </nav>
   );
 }
+
 
 export default Header;
