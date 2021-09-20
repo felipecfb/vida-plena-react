a08d2f25b6277cc9128adc2a95268885920b1c6b
Felipe da Costa <felipecfvb@gmail.com>
2021-09-20 10:46:58 -0300

    Adicionando code da página do livro

--- src/pages/Livro/index.tsx
+++ src/pages/Livro/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import {Helmet} from "react-helmet";
 
 import Header from '../../components/Header';
+import MainLivro from '../../components/MainLivro';
 
 function Livro(){
     return (
         <><Helmet>
             <title>Projeto Vida Plena - Livro</title>
         </Helmet>
-        <Header /></>
+        <Header />
+        <MainLivro />
+        </>
     )
 }
 
