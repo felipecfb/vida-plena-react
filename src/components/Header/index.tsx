918c02da7fc2cff4e91c86a7ff7bbf1958885c60
Felipe da Costa <felipecfvb@gmail.com>
2021-09-22 10:16:05 -0300

    Revert "removendo yarn.lock"
    This reverts commit 5485ac54ce018a83125fe8991a30261ba121f5e1.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,40 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "./Header.css";
 import logo from "../../assets/logo.png";
 import openMenuImg from "../../assets/menu.png"
 import closeMenuImg from "../../assets/close.png"
 
 function Header() {
-
-  const [ status, setStatus ] = useState('close')
-
   return (
-    <>
-    <nav>
-    <div
-     className="menu-mobile"
-      role="button"
-       onClick={() => setStatus(status === 'open' ? 'close' : 'open')}>
-         <ul className="menu">
-            <li className={status}></li>
-            <li className={status}></li>
-            <li className={status}></li>
-         </ul>
+    <nav className="header-nav">
+      <div className="header-logo">
+        <img src={logo} alt="" />
       </div>
+      <div className="header-nav-links">
+          <div className="header-menu">
+              <a href="/">Home</a>
+              <a href="/sobre">Sobre</a>
+              <a href="/login">Curso</a>
+          </div>
+      </div>
+
+      <div className="menu-mobile">
+      <img src={openMenuImg} alt="" />
+      <div className="menu-hamburguer">
+        <div className="menu-hamburguer-close">
+        <img src={closeMenuImg} alt="" />
+        </div>
+        <div className="menu-hamburguer-links">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Projeto</a></li>
+            <li><a href="#">Livro</a></li>
+            <li><a href="#">Curso</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
     </nav>
-    </>
   );
 }
 
